fd793e02bb606e1a02578806deb9aef14c55190c
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-23 18:51:04 -0700

    custom button finish

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -60,9 +60,7 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.adobe_outlined),
               label: const Text('Text Icon'),
             ),
-
-            // TODO: Custom Button
-
+            const CustomButton(),
             IconButton(onPressed: () {}, icon: const Icon(Icons.no_backpack)),
             IconButton(
               onPressed: () {},
@@ -75,6 +73,31 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
