ba528f5a3e592196ef2ba25a327e1995037df17a
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-23 22:30:35 -0700

    app tutorial finish

--- widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,12 +1,154 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final List<SlideInfo> slides = [
+  SlideInfo(
+    'Busca la comida',
+    'Do velit consequat mollit consequat magna non.',
+    'assets/images/1.png',
+  ),
+  SlideInfo(
+    'Entrega rapida',
+    'Enim veniam ut excepteur culpa proident ut ipsum essdawe id ullamco occaecat.',
+    'assets/images/2.png',
+  ),
+  SlideInfo(
+    'Disfruta la comida',
+    'Commodo qui elit ut consequat mollit cupidatat.',
+    'assets/images/3.png',
+  ),
+];
+
+class AppTutorialScreen extends StatefulWidget {
   static const String name = 'app_tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageViewController = PageController();
+  bool endReach = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pageViewController.addListener(() {
+      final page = pageViewController.page ?? 0;
+      if (!endReach && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReach = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageViewController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl,
+                  ),
+                )
+                .toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              onPressed: () => context.pop(),
+              child: const Text('Salir'),
+            ),
+          ),
+          if (endReach)
+            Positioned(
+              bottom: 30,
+              right: 30,
+              child: FadeInRight(
+                from: 15,
+                delay: const Duration(seconds: 1),
+                child: FilledButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('Comenzar'),
+                ),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(imageUrl),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              title,
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              caption,
+              style: captionStyle,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
