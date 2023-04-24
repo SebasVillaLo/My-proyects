64ef26f26505f48dbce09840d1137dd1ddf00765
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-23 18:06:36 -0700

    rutas basicas configuradas

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -54,7 +55,13 @@ class _CustomListTitle extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        //TODO: navegar a otras pantallas
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
