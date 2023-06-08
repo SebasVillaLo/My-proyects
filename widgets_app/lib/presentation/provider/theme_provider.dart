dfbfd8674bb5d150b87425aff4f5f3eacd8e1d77
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-06-08 17:36:45 -0500

    add app

---
+++ widgets_app/lib/presentation/provider/theme_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+final isDarkmodeProvider = StateProvider((Ref ref) => false);
+final colorListProvider = Provider((ref) => colorList);
+final selectedColorProvider = StateProvider((ref) => 0);
+final themeNotifierProvider =
+    StateNotifierProvider<ThemeNotifier, AppTheme>((ref) => ThemeNotifier());
+
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  ThemeNotifier() : super(AppTheme());
+
+  void toggleDarkmode() {
+    state = state.copyWith(isDarkmode: !state.isDarkmode);
+  }
+
+  void changeColorIndex(int colorIndex) {
+    state = state.copyWith(selectedColor: colorIndex);
+  }
+}
